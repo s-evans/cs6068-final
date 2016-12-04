007c0e6925cb101f76f913cb2fd75ad5dd963808
Sean Evans <ucn2523@oakley01.osc.edu>
2016-12-04 09:20:03 -0500

    code words being generated properly

--- huffman_tree.cu
+++ huffman_tree.cu
@@ -11,7 +11,7 @@ typedef struct _code_word_t {
 
 struct node_t {
     unsigned int weight;
-    unsigned short left_idx;
+    unsigned char left_idx;
     unsigned char symbol;
 };
 
@@ -37,11 +37,11 @@ __global__ void initialize_nodes(
     const unsigned int idx = threadIdx.x;
 
     if ( idx < populated_nodes ) {
-        nodes[idx].left_idx = USHRT_MAX;
+        nodes[idx].left_idx = UCHAR_MAX;
         nodes[idx].symbol = static_cast<short>( sorted_symbols[idx] );
         nodes[idx].weight = sorted_histogram[idx];
     } else {
-        nodes[idx].left_idx = USHRT_MAX;
+        nodes[idx].left_idx = UCHAR_MAX;
         nodes[idx].symbol = 0;
         nodes[idx].weight = 0;
     }
@@ -50,7 +50,7 @@ __global__ void initialize_nodes(
 
 std::ostream& operator<< ( std::ostream& stream, node_t const& node )
 {
-    return stream << "node.left_idx: " << static_cast<unsigned int>( node.left_idx ) 
+    return stream << "node.left_idx: " << static_cast<int>( node.left_idx == UCHAR_MAX ? -1 : node.left_idx * 2 ) 
         << "; node.symbol: "  << static_cast<unsigned int>( node.symbol ) 
         << "; node.weight: " << static_cast<unsigned int>( node.weight ) 
         << ";";
@@ -69,103 +69,131 @@ std::ostream& operator<< ( std::ostream& stream, code_word_t const& code_word )
     return stream << ";";
 }
 
-__global__ void insert_super_node (
-        node_t* const nodes,
-        unsigned int offset )
+__global__ void insert_super_nodes (
+        node_t* const pnodes )
 {
     __shared__ unsigned int not_moved;
     __shared__ unsigned int left_weight;
     __shared__ unsigned int right_weight;
     __shared__ unsigned int new_weight;
     const unsigned int idx = threadIdx.x;
+    const node_t* const end_nodes = pnodes + blockDim.x;
 
-    if ( idx == 0 ) {
-        not_moved = 0;
-        left_weight = nodes[0].weight;
-        right_weight = nodes[1].weight;
-        new_weight = left_weight + right_weight;
-    }
+    // TODO: update to use shared memory?
 
-    __syncthreads();
+    for ( node_t* nodes = pnodes ; nodes < end_nodes ; nodes += 2 ) {
+        const unsigned int offset = nodes - pnodes;
 
-    if ( left_weight == 0 || right_weight == 0 ) {
+        if ( idx == 0 ) {
+            not_moved = 0;
+            left_weight = nodes[0].weight;
+            right_weight = nodes[1].weight;
+            new_weight = left_weight + right_weight;
+        }
+
         __syncthreads();
-        return;
-    }
 
-    const unsigned int my_weight = nodes[idx].weight;
+        if ( left_weight == 0 || right_weight == 0 ) {
+            return;
+        }
 
-    if ( my_weight == 0 ) {
+        const unsigned int my_weight = nodes[idx].weight;
+
+        if ( my_weight == 0 ) {
+            return;
+        }
+
+        const bool move = ( my_weight >= new_weight );
+        node_t node;
+
+        if ( move ) {
+            node = nodes[idx];
+        } else {
+            atomicAdd( &not_moved, 1 );
+        }
+
         __syncthreads();
-        return;
-    }
 
-    const bool move = ( my_weight >= new_weight );
-    node_t node;
+        if ( move ) {
+            nodes[idx + 1] = node;
+        } else if ( idx == 0 ) {
+            node_t* const pnode = &nodes[not_moved];
+            pnode->left_idx = offset / 2;
+            pnode->symbol = 0;
+            pnode->weight = new_weight;
+        }
 
-    if ( move ) {
-        node = nodes[idx];
-    } else {
-        atomicAdd( &not_moved, 1 );
-    }
-
-    __syncthreads();
-
-    if ( move ) {
-        nodes[idx + 1] = node;
-    } else if ( idx == 0 ) {
-        node_t* const pnode = &nodes[not_moved];
-        pnode->left_idx = offset;
-        pnode->symbol = 0;
-        pnode->weight = new_weight;
+        __syncthreads();
     }
 }
 
+__device__ void init( unsigned int* const stack )
+{
+    stack[0] = 1;
+}
+
+__device__ void push(
+        unsigned int* const stack,
+        unsigned int const idx,
+        unsigned int const code_bit )
+{
+    const unsigned int ptr = stack[0];
+    stack[0] += 2;
+    stack[ptr] = idx;
+    stack[ptr + 1] = code_bit;
+}
+
+__device__ void pop(
+        unsigned int* const stack,
+        unsigned int* const idx,
+        unsigned int* const code_bit )
+{
+    const unsigned int ptr = stack[0] - 2;
+    stack[0] -= 2;
+    *idx = stack[ptr];
+    *code_bit = stack[ptr + 1];
+}
+
+__device__ bool empty( const unsigned int* const stack )
+{
+    return ( stack[0] == 1 );
+}
+
 __global__ void generate_code_words(
-    code_word_t* code_word_map,
+    code_word_t* const code_word_map,
     const node_t* const nodes )
 {
-    const unsigned int tid = threadIdx.x + blockDim.x * blockIdx.x;
-    const unsigned int init_idx = tid / 2;
-    const unsigned int thread_dir = tid % 2;
+    // TODO: use shared memory?
 
-    // TODO: fix this
+    const unsigned int tid = threadIdx.x;
+    const unsigned int direction = tid % 2;
+    unsigned int stack[1024];
 
-    /* for ( unsigned int depth_limit = 1 ; depth_limit < 3 ; ++depth_limit  ) { */
-    for ( unsigned int depth_limit = 1 ; depth_limit < blockDim.x / 2 ; ++depth_limit  ) {
+    if ( nodes[tid].weight == 0 ) {
+        return;
+    }
 
-        unsigned int idx = init_idx;
+    init( stack );
+    push( stack, tid, direction );
 
-        for ( unsigned int depth = 0 ; depth < depth_limit ; ++depth ) {
+    while ( !empty( stack ) ) {
 
-            if ( idx == USHRT_MAX ) {
-                continue;
-            }
+        unsigned int idx;
+        unsigned int code_bit;
 
-            if ( nodes[idx].weight == 0 ) {
-                idx = USHRT_MAX;
-                continue;
-            }
+        pop( stack, &idx, &code_bit );
 
-            if ( nodes[idx].left_idx == USHRT_MAX ) {
-                idx = USHRT_MAX;
-                continue;
-            }
-
-            idx = nodes[idx].left_idx + thread_dir;
-        }
-
-        if ( idx == USHRT_MAX ) {
+        if ( nodes[idx].left_idx == UCHAR_MAX ) {
+            const unsigned char symbol = nodes[idx].symbol;
+            atomicOr( &code_word_map[symbol].code, code_bit );
+            atomicAdd( &code_word_map[symbol].size, 1 );
             continue;
         }
 
-        if ( nodes[idx].left_idx != USHRT_MAX ) {
-            continue;
-        }
-
-        const unsigned char symbol = nodes[idx].symbol;
-        atomicOr( &code_word_map[symbol].code, thread_dir << ( depth_limit - 1 ) );
-        atomicAdd( &code_word_map[symbol].size, 1 );
+        idx = nodes[idx].left_idx * 2;
+        code_bit <<= 1;
+        push( stack, idx, code_bit );
+        push( stack, idx + 1, code_bit );
     }
 }
 
@@ -174,9 +202,6 @@ void make_huffman_tree(
         const unsigned int* const d_sorted_symbols)
 {
     const unsigned int histogram_size = 256;
-
-    // TODO: free device buffer eventually
-    // TODO: do something useful with the tree
 
     // TODO: rename function
     // TODO: rename file
@@ -198,7 +223,7 @@ void make_huffman_tree(
     checkCudaErrors( cudaStreamSynchronize( 0 ) );
 
     const unsigned int node_count = histogram_size - start_idx;
-    const unsigned int max_node_count = ( blelloch_size( node_count ) << 1 ) - 1;
+    const unsigned int max_node_count = ( blelloch_size( node_count ) << 1 );
 
     node_t* d_nodes;
     checkCudaErrors( cudaMalloc( &d_nodes, sizeof( *d_nodes ) * max_node_count ) );
@@ -208,28 +233,31 @@ void make_huffman_tree(
             d_nodes,
             node_count );
 
-    // TODO: move this loop to the kernel
-
-    for ( unsigned int i = 0 ; i < max_node_count ; i += 2 ) {
-        insert_super_node<<<1, max_node_count - i, 0, 0>>>( &d_nodes[i], i );
-    }
-
-    generate_code_words<<<1, max_node_count * 2, 0, 0>>>( d_code_word_map, d_nodes );
-
     /* std::cerr << "node_count: " << node_count << std::endl; */
     /* std::cerr << "max_node_count: " << max_node_count << std::endl; */
 
-    checkCudaErrors( cudaStreamSynchronize( 0 ) );
-    for ( unsigned int i = 0 ; i < max_node_count ; ++i ) {
-        node_t tmp;
-        checkCudaErrors( cudaMemcpy( &tmp, &d_nodes[i], sizeof( tmp ), cudaMemcpyDeviceToHost ) );
-        std::cerr << "idx: " << i << "; " << tmp << std::endl;
-    }
+    insert_super_nodes<<<1, max_node_count, 0, 0>>>( d_nodes );
 
-    checkCudaErrors( cudaStreamSynchronize( 0 ) );
-    for ( unsigned int i = 0 ; i < UCHAR_MAX ; ++i ) {
-        code_word_t tmp;
-        checkCudaErrors( cudaMemcpy( &tmp, &d_code_word_map[i], sizeof( tmp ), cudaMemcpyDeviceToHost ) );
-        std::cerr << "idx: " << i << "; " << tmp << std::endl;
-    }
+    /* checkCudaErrors( cudaStreamSynchronize( 0 ) ); */
+    /* for ( unsigned int i = 0 ; i < max_node_count ; ++i ) { */
+    /*     node_t tmp; */
+    /*     checkCudaErrors( cudaMemcpy( &tmp, &d_nodes[i], sizeof( tmp ), cudaMemcpyDeviceToHost ) ); */
+    /*     std::cerr << "idx: " << i << "; " << tmp << std::endl; */
+    /* } */
+
+    generate_code_words<<<1, max_node_count, 0, 0>>>( d_code_word_map, d_nodes );
+
+    /* checkCudaErrors( cudaStreamSynchronize( 0 ) ); */
+    /* for ( unsigned int i = 0 ; i < UCHAR_MAX ; ++i ) { */
+    /*     code_word_t tmp; */
+    /*     checkCudaErrors( cudaMemcpy( &tmp, &d_code_word_map[i], sizeof( tmp ), cudaMemcpyDeviceToHost ) ); */
+    /*     std::cerr << "idx: " << i << "; " << tmp << std::endl; */
+    /* } */
+
+    generate_output<<<1, max_node_count, 0, 0>>>( d_code_word_map, d_nodes );
+
+    // TODO: must output the code word table as well
+
+    checkCudaErrors( cudaFree( d_code_word_map ) );
+    checkCudaErrors( cudaFree( d_nodes ) );
 }
